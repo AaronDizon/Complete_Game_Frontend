4c89cdf7ef548994489f0978810ddda1feb2de3e
Aaron <imartindizon@gmail.com>
2022-01-10 14:15:17 -0800

    wip: context

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { UserContext } from '.context/UserContext'
+import { UserProvider } from '.context/UserContext';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <UserContext>
+      <UserProvider>
         <App />
-      </UserContext>
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
