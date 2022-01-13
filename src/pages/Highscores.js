90e9bef9fb023d75c49d2bc2644ac22b11797a1d
Aaron <imartindizon@gmail.com>
2022-01-12 17:01:49 -0800

    wip: highscore page, almost got it

--- src/pages/Highscores.js
+++ src/pages/Highscores.js
@@ -15,8 +15,23 @@ const Highscores = () => {
         try {
             const response = await axios.get(`${env.BACKEND_URL}/score/all`)
             const scoresArray = response.data
+            const sortedArray = [{"score":0}]
+
+            for (let i = 0; i < scoresArray.length; i++) {
+                for (let j =0; j < scoresArray.length; j++){
+                    if (scoresArray[i].score > sortedArray[j].score){
+                        sortedArray.splice(j, 0, scoresArray[i])
+                        break
+                    }
+                }
+            }
             console.log(scoresArray)
-            
+            console.log(sortedArray)
+
+            let topTen = sortedArray.slice(0,10)
+            topTen.pop()
+
+            console.log(topTen)
         } catch (err) {
             console.log(err)
         }
